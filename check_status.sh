da80f5c72b8d0f7629b9d857e76deb2c37bd8031
NaotakaSaito <saitou088@dsn.lapis-semi.com>
2016-03-28 23:29:28 +0900

    add check_status and setup.sh

---
+++ check_status.sh
@@ -0,0 +1,34 @@
+#! /bin/sh
+echo '****************************************'
+echo '******  check git status ***************'
+echo '****************************************'
+echo ''
+echo ''
+echo ' Check LazDriver'
+cd ~/develop/LazDriver
+git branch
+git status
+echo ''
+echo ''
+echo ' Check LazuriteJava'
+cd ~/develop/LazuriteJava
+git branch
+git status
+echo ''
+echo ''
+echo ' Check LazuriteGraph'
+cd ~/develop/LazuriteGraph
+git branch
+git status
+echo ''
+echo ''
+echo ' Check LazuriteInstaller'
+cd ~/develop/LazuriteInstaller
+git branch
+git status
+echo ''
+echo ''
+echo ' Check LazGem'
+cd ~/develop/LazGem
+git branch
+git status
--- setup.sh
+++ setup.sh
@@ -2,3 +2,7 @@
 sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get install vim ruby1.9.3 jfbterm uim uim-anthy samba xrdp
+cp -r .bashrc .vim .vimrc .viminfo ~/
+sudo cp smb.conf /etc/samba/
+sudo service samba restart
+
