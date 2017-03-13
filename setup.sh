bbde3ea456d6a636b7f60b3c7c26e647c5bb9d00
NaotakaSaito <saitou088@dsn.lapis-semi.com>
2017-03-13 22:35:00 +0900

    update

--- setup.sh
+++ setup.sh
@@ -5,9 +5,6 @@ sudo apt-get update
 #sudo apt-get upgrade
 sudo apt-get install vim vim-gnome jfbterm uim uim-anthy ctags obconf
 
-cd ~
-curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
-
 mkdir -p ~/.vim/dein/repos/github.com/Shougo/dein.vim
 git clone https://github.com/Shougo/dein.vim.git ~/.vim/dein/repos/github.com/Shougo/dein.vim
 
