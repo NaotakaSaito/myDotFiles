a6a46d0c58da0f94e5a07bcdc3815363d8d7b440
NaotakaSaito <saitou088@dsn.lapis-semi.com>
2017-03-13 09:17:18 +0900

    update

--- setup.sh
+++ setup.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
-#sudo apt-get update
+HOMEDIR=$(pwd)
+echo $HOMEDIR
+sudo apt-get update
 #sudo apt-get upgrade
-sudo apt-get install vim vim-gnome jfbterm uim uim-anthy ctags
+sudo apt-get install vim vim-gnome jfbterm uim uim-anthy ctags obconf
 
 cd ~
 curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
@@ -9,6 +11,7 @@ curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh >
 mkdir -p ~/.vim/dein/repos/github.com/Shougo/dein.vim
 git clone https://github.com/Shougo/dein.vim.git ~/.vim/dein/repos/github.com/Shougo/dein.vim
 
+cd $HOMEDIR
 cp .bash_aliases .vimrc ~/
 mkdir -p ~/.vim/rc
 cp  dein.toml  dein_lazy.toml ~/.vim/rc/
