77a4ef8fe0e81f4859b39ba6d640209eb215f58c
NaotakaSaito <saitou088@dsn.lapis-semi.com>
2017-09-04 16:39:12 +0900

    correct typo in setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ HOMEDIR=$(pwd)
 echo $HOMEDIR
 sudo apt-get update
 sudo apt-get upgrade -y
-sudo apt-get install -y vim vim-gtk fonts-ipafont ttf-kochi-gothic xfonts-intl-japanese xfonts-intl-japanese-big xfont-kaname uim uim-anthy ctags obconf x11vnc node-gyp npm
+sudo apt-get install -y vim vim-gtk fonts-ipafont ttf-kochi-gothic xfonts-intl-japanese xfonts-intl-japanese-big xfonts-kaname uim uim-anthy ctags obconf x11vnc node-gyp npm
 
 mkdir -p ~/.vim/dein/repos/github.com/Shougo/dein.vim
 git clone https://github.com/Shougo/dein.vim.git ~/.vim/dein/repos/github.com/Shougo/dein.vim
