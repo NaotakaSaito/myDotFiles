84f1f0cf37c9355882f5b3f4a7fdeadb60bd857a
NaotakaSaito <saitou088@dsn.lapis-semi.com>
2018-04-03 13:18:37 +0900

    update

--- mount.sh
+++ mount.sh
@@ -1,5 +1,2 @@
-先にマウントポジション(/mnt/www-lazurite-ioなど)をmkdirで作成しておく
-起動するたびに、以下のコマンドを実行する
-
-mount -t cifs //192.168.30.11/www /mnt/www-lazurite-io -o user=root
-mount -t cifs //192.168.30.16/api /mnt/api-lazurite-io -o user=pi
+sudo mount -t cifs //192.168.30.11/www /mnt/www-lazurite-io -o user=root
+sudo mount -t cifs //192.168.30.16/api /mnt/api-lazurite-io -o username=pi,password=@Lazurite920,vers=1.0
