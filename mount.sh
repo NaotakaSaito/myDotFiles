a8b3a7540faa0129630a92f95e38bfad27238bcf
NaotakaSaito <saitou088@dsn.lapis-semi.com>
2018-05-31 17:41:02 +0900

    update

--- mount.sh
+++ mount.sh
@@ -1,2 +1,2 @@
-sudo mount -t cifs //192.168.30.11/www /mnt/www-lazurite-io -o user=root
+sudo mount -t cifs //192.168.30.19/www /mnt/www-lazurite-io -o username=root,password=@Lazurite920,vers=1.0
 sudo mount -t cifs //192.168.30.16/api /mnt/api-lazurite-io -o username=pi,password=@Lazurite920,vers=1.0
